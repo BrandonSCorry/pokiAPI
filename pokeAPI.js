d0a9a570725f93cd52f01f12680187fd50e38378
Poltergeist <brandonssc@gmail.com>
2018-09-05 13:37:18 -0400

    more fixin

--- pokeAPI.js
+++ pokeAPI.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 
   $.ajax(settings).then(function (response) {
 
-    var results = response.data;
+    var results = response.results;
     console.log(results);
     console.log(response);
   });
